e7f8a7fc6be4b19251268b6903fe5c19d63a0fab
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-08-06 17:29:05 -0500

    added reflect example

---
+++ src/github.com/mvolkmann/reflection/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type any interface{}
+
+func whatAmI(value any) {
+	kind := reflect.TypeOf(value).Kind()
+	switch kind {
+	case reflect.Array:
+		fmt.Println("I am an array.")
+	case reflect.Slice:
+		fmt.Println("I am a slice.")
+	default:
+		fmt.Println("I am something else.")
+	}
+}
+
+func main() {
+	s := []int{1, 2, 3}
+	whatAmI(s)
+
+	a := [3]int{1, 2, 3}
+	whatAmI(a)
+
+	i := 1
+	whatAmI(i)
+}
