9c3b9b616467699e48ee126e020e4fa6389e3194
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-08-10 19:41:48 -0500

    added Floats function to mapover package

--- src/github.com/mvolkmann/mapover-demo/mapover.go
+++ src/github.com/mvolkmann/mapover-demo/mapover.go
@@ -1,14 +1,22 @@
 package mapover
 
-// IntToIntFn is the type of the functions
-// passed to MapOverInts as the second argument.
-type IntToIntFn = func(int) int
-
-// Ints takes a slice of int values
-// and a function that takes an int and returns an int.
+// Floats takes a slice of float64 values and
+// a function that takes that type and returns that type.
 // It returns a new slice that is the result of
 // applying the function to each element in the slice.
-func Ints(arr []int, fn IntToIntFn) []int {
+func Floats(arr []float64, fn func(float64) float64) []float64 {
+	result := make([]float64, len(arr))
+	for i, v := range arr {
+		result[i] = fn(v)
+	}
+	return result
+}
+
+// Ints takes a slice of int values and
+// a function that takes that type and returns that type.
+// It returns a new slice that is the result of
+// applying the function to each element in the slice.
+func Ints(arr []int, fn func(int) int) []int {
 	result := make([]int, len(arr))
 	for i, v := range arr {
 		result[i] = fn(v)
--- src/github.com/mvolkmann/mapover-demo/mapover_test.go
+++ src/github.com/mvolkmann/mapover-demo/mapover_test.go
@@ -5,10 +5,19 @@ import (
 	"testing"
 )
 
+func Test_Floats(t *testing.T) {
+	values := []float64{1.2, 2.3, 4.5}
+	expected := []float64{2.4, 4.6, 9.0}
+	actual := Floats(values, func(n float64) float64 { return n * 2.0 })
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("mapOverInts expected %v but got %v", expected, actual)
+	}
+}
+
 func Test_Ints(t *testing.T) {
-	ints := []int{1, 2, 4}
+	values := []int{1, 2, 4}
 	expected := []int{2, 4, 8}
-	actual := Ints(ints, func(n int) int { return n * 2 })
+	actual := Ints(values, func(n int) int { return n * 2 })
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("mapOverInts expected %v but got %v", expected, actual)
 	}
